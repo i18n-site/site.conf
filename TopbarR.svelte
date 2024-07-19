ed60308090667fa1325f78cb55092cb675aef28e
i18n <i18n.site@gmail.com>
2024-07-19 12:52:52 +0800

    🔥

---
+++ TopbarR.svelte
@@ -0,0 +1,25 @@
+<script lang="coffee">
+> @8p/full:Full
+  @8p/lang/Lang.svelte
+
+# ./Search.svelte
+</script>
+
+<template lang="pug">
+//- Search
+aside
+  Full
+  Lang
+</template>
+
+<style lang="stylus">
+@media (max-width 560px)
+  aside
+    display flex
+    justify-content space-between
+    width 100%
+
+    &>:global(i:last-child)
+      margin-right 0 !important
+</style>
+
