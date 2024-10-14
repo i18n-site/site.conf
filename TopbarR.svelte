5254d382f59d4db4e53db0ced4a8fdc8424fbb21
i18n <i18n.site@gmail.com>
2024-10-14 11:05:47 +0800

    🔥

---
+++ TopbarR.svelte
@@ -0,0 +1,15 @@
+<script lang="coffee">
+> @8p/full:Full
+  @8p/lang/Lang.svelte
+  @8p/qy/Search.svelte
+</script>
+
+<template lang="pug">
+Search
+aside
+  Full
+  Lang
+</template>
+
+<style lang="stylus"></style>
+
---
+++ use.js
@@ -0,0 +1,12 @@
+import { USE } from "x/_.js"
+import Md from "@8p/page-md"
+import Toc from "@8p/page-toc"
+import Doc from "@8p/page-doc"
+import Blog from "@8p/page-blog"
+
+Object.assign(USE, {
+	Md,
+	Toc,
+	Doc,
+	Blog,
+})
